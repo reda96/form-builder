e4a1d463322039a2441bb6b9018fff8fae833471
reda96 <reda96>
2025-02-03 15:28:18 +0200

    add code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,98 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {  jsonForm } from '../assets/form';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { debounceTime, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'form-builder';
+  jsonForm = jsonForm;
+  currentBox = null;
+  formBuilder = new FormGroup({});
+  currentField ={
+    type:"",
+    id:"",
+    currentFieldForm: new FormGroup({
+    name: new FormControl('', [Validators.required,Validators.minLength(5), Validators.maxLength(10)]),
+    id:  new FormControl('', [Validators.required]),
+    borderColor:  new FormControl(''),
+    borderSize:  new FormControl(''),
+
+    caption:  new FormControl(''),
+    placeholder:  new FormControl(''),
+    rows:  new FormControl(''),
+    cols:  new FormControl(''),
+
+  })
+}
+  ngOnInit(): void {
+  
+    
+    this.fillForm(jsonForm, this.formBuilder);
+
+    this.currentField.currentFieldForm.valueChanges.pipe(debounceTime(500)).subscribe((values)=> {
+      // console.log(this.currentField.id,values.id);
+      
+      if(this.currentField.id==values.id)
+      { 
+        let itemIndex =jsonForm.findIndex(field => field.id == this.currentField.id);
+
+        if(this.jsonForm[itemIndex].name && values.name)
+          this.jsonForm[itemIndex].name = values.name;
+
+        if(this.jsonForm[itemIndex].placeholder && values.placeholder)
+          this.jsonForm[itemIndex].placeholder = values.placeholder;
+
+        if(this.jsonForm[itemIndex].border.size && values.borderSize)
+        this.jsonForm[itemIndex].border.size = values.borderSize;
+
+        if(this.jsonForm[itemIndex].border.color && values.borderColor)
+          this.jsonForm[itemIndex].border.color = values.borderColor;
+       
+        if(this.jsonForm[itemIndex].caption && values.caption)
+          this.jsonForm[itemIndex].caption = values.caption;
+
+
+        if(this.jsonForm[itemIndex].rows && values.rows)
+          this.jsonForm[itemIndex].rows = +values.rows;
+
+        if(this.jsonForm[itemIndex].cols && values.cols)
+          this.jsonForm[itemIndex].cols = +values.cols;
+
+        
+      }
+    });
+
+    
+  }
+  fillForm(json:any[], formBuilder:FormGroup) {
+  for(let i=0; i<json.length;i++)
+   formBuilder.addControl(json[i].name, new FormControl('', Validators.required))
+
+  }
+  selectCurrentField(currentField:any) {
+    //  console.log(currentField);
+    this.currentField.type = currentField.type;
+    this.currentField.id = currentField.id;
+    const currentFieldForm =this.currentField.currentFieldForm as FormGroup;
+    currentFieldForm.controls['name'].setValue(currentField.name);
+    currentFieldForm.controls['id'].setValue(currentField.id);
+    currentFieldForm.controls['borderColor'].setValue(currentField.border.color);
+    currentFieldForm.controls['borderSize'].setValue(currentField.border.size);
+
+    currentFieldForm.controls['caption']?.setValue(currentField.caption);
+    currentFieldForm.controls['placeholder']?.setValue(currentField.placeholder);
+
+    currentFieldForm.controls['rows']?.setValue(currentField.rows);
+    currentFieldForm.controls['cols']?.setValue(currentField.cols);
+   
+    
+  }
+
+
+
 }
---
+++ src/assets/form.ts
@@ -0,0 +1,55 @@
+export const jsonForm =[
+    {
+      "type": "textbox",
+      "name": "firstTextbox",
+      "id": "c4c9f16d-7d1b-47c4-b623-34908be3f94e", 
+      "border": {
+        "color": "black",
+        "size": "1px"
+      },
+      "placeholder": "Enter text here"
+    },
+    {
+      "type": "textarea",
+      "name": "firstTextarea",
+      "id": "b3d25b74-b678-4b1f-bff9-bd2fe7bb7b6f",
+      "border": {
+        "color": "blue",
+        "size": "1px"
+      },
+      "rows": 5,
+      "cols": 30
+    },
+    {
+      "type": "button",
+      "name": "firstButton",
+      "id": "ae6fa48e-fc92-4d0b-8c67-d1cf179dfb8a",
+      "border": {
+        "color": "green",
+        "size": "none"
+      },
+      "caption": "Click Me"
+    },
+    {
+      "type": "textarea",
+      "name": "secondTextarea",
+      "id": "a2871f9e-c79c-4a39-8b5b-909b77bffb33",
+      "border": {
+        "color": "red",
+        "size": "2px"
+      },
+      "rows": 8,
+      "cols": 40
+    },
+    {
+      "type": "textarea",
+      "name": "thirdTextarea",
+      "id": "99fa5fd8-c905-4f0c-8e88-d98da578736f",
+      "border": {
+        "color": "purple",
+        "size": "1px"
+      },
+      "rows": 6,
+      "cols": 35
+    }
+  ]
